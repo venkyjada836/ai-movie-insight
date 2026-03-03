efb137c8d4c74a9a95cd093234d69676154bc8de
venkyjada836 <venkyjada836@gmail.com>
2026-03-03 22:15:16 +0530

    Fixed Tailwind configuration properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
